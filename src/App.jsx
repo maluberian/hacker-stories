4391141cd4d58b2169c9127708cca1b087eab8c1
Dustin Clifford <dclifford@sitesoftllc.net>
2024-10-17 17:54:26 -0400

    moving to reducer for state

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import * as React from 'react';
-
-import {Divider, DatePicker, Button} from "antd";
+import {Divider} from "antd";
 
 import './App.css'
-import {useState} from "react";
+import {useReducer, useState, useEffect} from "react";
 
-const Item = ({key, url, title, authors, num_comments, points}) => {
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search'
 
+const STORY_ACTIONS = {
+    STORIES_FETCH_INIT: 'STORIES_FETCH_INIT',
+    STORIES_FETCH_SUCCESS: 'STORIES_FETCH_SUCCESS',
+    STORES_FETCH_FAILURE: 'STORES_FETCH_FAILURE',
+    SET_STORIES: 'SET_STORIES',
+    REMOVE_STORY: 'REMOVE_STORY',
+}
+
+const Item = ({key, item, onRemoveStory}) => {
     return (
         <li key={key}>
-            <span><a href={url}>{title}</a></span>
-            <span>{`${authors}`}</span>
-            <span>{num_comments} comments</span>
-            <span>{points} points</span>
+            <span><a href={item.url}>{item.title}</a></span>
+            <span>{`${item.author}`}</span>
+            <span>{item.num_comments} comments</span>
+            <span>{item.points} points</span>
+            <button onClick={() => {
+                onRemoveStory(item)
+            }}>Dismiss
+            </button>
         </li>
     )
 }
 
-const List = (props) => {
+const List = ({stories, onRemoveStory}) => {
+    console.log("list: ", stories)
     return (
-        <ul>
-            {props.list.map(({object_id, ...item}) => {
+        <ul key="stories">
+            {stories.map((item) => {
                 return (
-                    <Item key={object_id} {...item} />
+                    <Item key={item.objectID} item={item} onRemoveStory={onRemoveStory}/>
                 )
             })}
         </ul>
@@ -30,7 +42,7 @@ const List = (props) => {
 }
 
 const InputWithLabel = ({id, type = "text", value, isFocused, onInputChange, children}) => {
-
+    console.log("input label: ", value)
     return (
         <>
             <label htmlFor="search">{children}</label>
@@ -39,66 +51,119 @@ const InputWithLabel = ({id, type = "text", value, isFocused, onInputChange, chi
     );
 }
 
-const App = () => {
-    const list = [
-        {
-            title: 'React',
-            url: 'https://reactjs.org',
-            authors: ['Jordan Walker'],
-            num_comments: 3,
-            points: 4,
-            object_id: 0,
-        },
-        {
-            title: 'Redux',
-            url: 'https://redux.js.org',
-            authors: ['Dan Abramov', 'Andrew Clark'],
-            num_comments: 2,
-            points: 5,
-            object_id: 1,
-        },
-    ]
-
-
-    const [logMessage, setLogMessage] = useLogMessage()
-    const handleLogMessage = (event) => { setLogMessage(event.target.value) }
-
-    const [searchTerm, setSearchTerm] = useStorageState('search','React')
-    const handleSearch = (event) => {
-        console.log(event.target.value)
-        setSearchTerm(event.target.value)
-    }
-
-    const searchedStories = list.filter((s) => {
-        return s.title.toLowerCase().includes(searchTerm.toLowerCase())
-    })
-    return (
-        <div>
-            <InputWithLabel id="search" isFocused onInputChange={handleSearch} search={searchTerm}><strong>Search:</strong></InputWithLabel>
-            <InputWithLabel id="logMessage" type="url" onInputChange={handleLogMessage} search={logMessage}><strong>Message:</strong></InputWithLabel>
-            <hr/>
-            <List list={searchedStories} />
-            <Divider/>
-            <Button variant={"text"} iconPosition={"end"} loading={true}>Test</Button>
-            <Divider/>
-            <DatePicker/>
-        </div>
-    );
-}
-
 const useLogMessage = () => {
-    const[msg, setMsg] = useState('--blank message--')
-    React.useEffect(() => { console.log(msg)}, [msg])
+    const [msg, setMsg] = useState('--blank message--')
+    useEffect(() => {
+        console.log(msg)
+    }, [msg])
     return [msg, setMsg]
 }
 
 const useStorageState = (key, initialState) => {
     const [value, setValue] = useState(localStorage.getItem(key) ?? initialState)
-    React.useEffect(() => {
+    useEffect(() => {
         localStorage.setItem(key, value)
     }, [value, key])
     return [value, setValue]
 
 }
 
+const App = () => {
+    const storiesReducer = (state, action) => {
+        switch (action.type) {
+            case STORY_ACTIONS.STORIES_FETCH_INIT:
+                return {
+                    ...state,
+                    isLoading: true,
+                    isError: false,
+                }
+            case STORY_ACTIONS.STORIES_FETCH_SUCCESS:
+                return {
+                    ...state,
+                    data: action.payload,
+                    isLoading: false,
+                    isError: false,
+                }
+            case STORY_ACTIONS.STORES_FETCH_FAILURE:
+                return {
+                    ...state,
+                    isLoading: false,
+                    isError: true,
+                }
+            case STORY_ACTIONS.REMOVE_STORY:
+                console.log("remove: ", state, action)
+                return {
+                    ...state,
+                    data: state.data.filter((story) => {
+                        console.log("remove-filter: ", story.objectID, action.payload.objectID)
+                        return story.objectID !== action.payload.objectID
+                    })
+                }
+            default:
+                throw new Error()
+        }
+    }
+
+    // load the stories
+    useEffect(() => {
+        dispatchStories({type: STORY_ACTIONS.STORIES_FETCH_INIT})
+
+        fetch(`${API_ENDPOINT}?query=react`)
+            .then(res => res.json())
+            .then(json => {
+                console.log("result: ", json)
+                dispatchStories({
+                    type: STORY_ACTIONS.STORIES_FETCH_SUCCESS,
+                    payload: json.hits,
+                })
+            })
+            .catch((err) => {
+                console.error("error: ", err)
+                dispatchStories({type: STORY_ACTIONS.STORES_FETCH_FAILURE})
+            })
+    }, [])
+
+    const handleRemoveStory = (item) => {
+        dispatchStories({type: STORY_ACTIONS.REMOVE_STORY, payload: item})
+    }
+
+    const [logMessage, setLogMessage] = useLogMessage()
+    const handleLogMessage = (event) => {
+        setLogMessage(event.target.value)
+    }
+
+    const [searchTerm, setSearchTerm] = useStorageState('search', 'React')
+    const handleSearch = (event) => {
+        console.log(event.target.value)
+        setSearchTerm(event.target.value)
+    }
+
+    const [stories, dispatchStories] = useReducer(storiesReducer, {data: [], isLoading: false, isError: false})
+
+    const searchedStories = stories.data.filter((s) => {
+        console.log("filter: ", s)
+        return s.title.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+
+    console.log("state: ", stories)
+    return (
+        <div>
+            {stories.isError && <p>Something went wrong!</p>}
+            {stories.isLoading ?
+                (<p>Loading...</p>) :
+                (
+                    <>
+                        <InputWithLabel id="search" isFocused onInputChange={handleSearch}
+                                        value={searchTerm}><strong>Search:</strong></InputWithLabel>
+                        <InputWithLabel id="logMessage" type="url" onInputChange={handleLogMessage} value={logMessage}>
+                            <strong>Message:</strong>
+                        </InputWithLabel>
+                        <Divider/>
+                        <List stories={searchedStories} onRemoveStory={handleRemoveStory}/>
+                    </>
+                )
+            }
+        </div>
+    )
+}
 export default App
